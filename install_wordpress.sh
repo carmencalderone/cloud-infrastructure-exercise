5f7e8bc04ea74ebdb8b85489940af7a3666d24d3
Carmen <carmen.cald96@gmail.com>
2023-02-24 18:35:13 +0100

    AWS for WordPress initialization

---
+++ install_wordpress.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+sudo apt install apache2
+sudo systemctl start apache2
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip
+sudo mkdir -p /srv/www
+sudo chown www-data: /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+
+echo "<VirtualHost *:80>
+    DocumentRoot /srv/www/wordpress
+    <Directory /srv/www/wordpress>
+        Options FollowSymLinks
+        AllowOverride Limit Options FileInfo
+        DirectoryIndex index.php
+        Require all granted
+    </Directory>
+    <Directory /srv/www/wordpress/wp-content>
+        Options FollowSymLinks
+        Require all granted
+    </Directory>
+</VirtualHost>" | sudo tee -a /etc/apache2/sites-available/wordpress.conf
+
+echo "<VirtualHost *:80>
+    ServerName hostname.example.com
+    ... # the rest of the VHost configuration
+</VirtualHost>" | sudo tee -a /etc/hosts
+
+sudo service apache2 reload
+#### Start mysql and set root password
+
+sudo systemctl enable mysql
+sudo systemctl start mysql
+
+sudo /usr/bin/mysql -e "CREATE DATABASE wordpress;"
+sudo /usr/bin/mysql -e "CREATE USER wordpress@localhost IDENTIFIED BY '<your-password>';"
+####Install PHP
+sudo apt -y install php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml
+sudo systemctl restart apache2 
+
+sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/mysql/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/admin/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/admin/<your-password>/' /srv/www/wordpress/wp-config.php
---
+++ main.tf
@@ -0,0 +1,29 @@
+#deploy of the network AWS
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+  region="eu-south-1"
+}
+#deploy of subnet into network create below 
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-south-1a"
+}
+
+resource "aws_network_interface" "my_networkinterface" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+}
+
+  #deploy a virtual machine with 1 vCPU and 1,0 gb of RAM 
+resource "aws_instance" "wordpress_instance" {
+  ami= "ami-0b6321d6ee7c8ab67" # amazon linux AMI on MILAN region
+
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.my_networkinterface.id
+  }
+
+}
+
