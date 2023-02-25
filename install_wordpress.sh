2aec2593cc494ccdcb8308a1d5a99e1c549ce6aa
Carmen <carmen.cald96@gmail.com>
2023-02-25 20:00:11 +0100

    update a

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+################################################
+#####   Installation of Apache Web Server  #####
+################################################
 sudo apt install apache2
 sudo systemctl start apache2
 sudo apt install apache2 \
@@ -15,10 +19,14 @@ sudo apt install apache2 \
                  php-mysql \
                  php-xml \
                  php-zip
+
+
+#####   Creation and Configuration of the Directory for WordPress  #####
 sudo mkdir -p /srv/www
 sudo chown www-data: /srv/www
-curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www #Download the latest version of WP into the /srv/www directory
 
+#####   Update WordPress config file  #####
 echo "<VirtualHost *:80>
     DocumentRoot /srv/www/wordpress
     <Directory /srv/www/wordpress>
@@ -26,31 +34,36 @@ echo "<VirtualHost *:80>
         AllowOverride Limit Options FileInfo
         DirectoryIndex index.php
         Require all granted000
-    </Dir.............ectory>
+    </Directory>
     <Directory /srv/www/wordpress/wp-content>
         Options FollowSymLinks
         Require all granted
     </Directory>
 </VirtualHost>" | sudo tee -a /etc/apache2/sites-available/wordpress.conf
 
+#####   Update hosts file  #####
 echo "<VirtualHost *:80>
     ServerName hostname.example.com
     ... # the rest of the VHost configuration
 </VirtualHost>" | sudo tee -a /etc/hosts
 
 sudo service apache2 reload
-#### Start mysql and set root password
+
+##### Enable MySQL DBMS #####
 
 sudo systemctl enable mysql
 sudo systemctl start mysql
 
 sudo /usr/bin/mysql -e "CREATE DATABASE wordpress;"
 sudo /usr/bin/mysql -e "CREATE USER wordpress@localhost IDENTIFIED BY '<your-password>';"
-####Install PHP
+
+##### Install PHP #####
+
 sudo apt -y install php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml
 sudo systemctl restart apache2 
 
+##### Copy the config file into WP config file  #####
 sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
-sudo -u www-data sed -i 's/mysql/wordpress/' /srv/www/wordpress/wp-config.php
+##### Configure username and password  #####
 sudo -u www-data sed -i 's/admin/wordpress/' /srv/www/wordpress/wp-config.php
 sudo -u www-data sed -i 's/admin/<your-password>/' /srv/www/wordpress/wp-config.php
--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-#deploy of the network AWS
+#Deploy of the AWS network 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
   region="eu-south-1"
 }
-#deploy of subnet into network create below 
+#Deploy of subnet into network create below 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = "eu-south-1a"
 }
-
+#Deploy of network interface for the vm 
 resource "aws_network_interface" "my_networkinterface" {
   subnet_id   = aws_subnet.my_subnet.id
   private_ips = ["172.16.10.100"]
 }
 
-  #deploy a virtual machine with 1 vCPU and 1,0 gb of RAM 
+#Deploy a virtual machine with 1 vCPU and 1 GB of RAM Memory
 resource "aws_instance" "wordpress_instance" {
-  ami= "ami-0b6321d6ee7c8ab67" # amazon linux AMI on MILAN region 
+  ami= "ami-0b6321d6ee7c8ab67" # Amazon Linux 2 AMI on MILAN region 
   user_data = file("startup.sh")
   instance_type = "t2.micro"
   network_interface {
